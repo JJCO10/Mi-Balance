e4a739be767963d27eccc1c8e4a10eecffa35825
JJCO10 <juanjosecarmonaortiz@gmail.com>
2025-07-05 19:40:30 -0500

    modificaciones en las vistas

--- mi_balance/lib/views/dashboard/management/management_screen.dart
+++ mi_balance/lib/views/dashboard/management/management_screen.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'income_types_screen.dart';
+import 'expense_types_screen.dart';
 
-class ManagementScreen extends StatelessWidget {
+class ManagementScreen extends StatefulWidget {
   const ManagementScreen({super.key});
 
   @override
+  State<ManagementScreen> createState() => _ManagementScreenState();
+}
+
+class _ManagementScreenState extends State<ManagementScreen> {
+  bool showIncomeTypes = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/income-types');
-          },
-          child: const Text('Income Types'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/expense-types');
-          },
-          child: const Text('Expense Types'),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Management')),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => setState(() => showIncomeTypes = true),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: showIncomeTypes ? Colors.blue : Colors.grey,
+                ),
+                child: const Text('Income Types'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () => setState(() => showIncomeTypes = false),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: !showIncomeTypes ? Colors.blue : Colors.grey,
+                ),
+                child: const Text('Expense Types'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: showIncomeTypes
+                ? const IncomeTypesScreen()
+                : const ExpenseTypesScreen(),
+          ),
+        ],
+      ),
     );
   }
 }
--- mi_balance/lib/views/dashboard/transactions/transactions_screen.dart
+++ mi_balance/lib/views/dashboard/transactions/transactions_screen.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
+import 'incomes_screen.dart';
+import 'expenses_screen.dart';
 
-class TransactionsScreen extends StatelessWidget {
+class TransactionsScreen extends StatefulWidget {
   const TransactionsScreen({super.key});
 
   @override
+  State<TransactionsScreen> createState() => _TransactionsScreenState();
+}
+
+class _TransactionsScreenState extends State<TransactionsScreen> {
+  bool showIncomes = true;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/incomes');
-          },
-          child: const Text('Incomes'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/expenses');
-          },
-          child: const Text('Expenses'),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Transactions')),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => setState(() => showIncomes = true),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: showIncomes ? Colors.blue : Colors.grey,
+                ),
+                child: const Text('Incomes'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () => setState(() => showIncomes = false),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: !showIncomes ? Colors.blue : Colors.grey,
+                ),
+                child: const Text('Expenses'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: showIncomes ? const IncomesScreen() : const ExpensesScreen(),
+          ),
+        ],
+      ),
     );
   }
 }
